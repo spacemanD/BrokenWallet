a714f480c7206bee09e9fc08367fe9a3b6f3868e
SpacemanD <dimasforus@gmail.com>
2022-05-14 20:51:39 +0300

    Heroku deploy

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -12,9 +12,10 @@ namespace Persistence
         public static async Task SeedData(DataContext context,
             UserManager<AppUser> userManager)
         {
-            if (!userManager.Users.Any() && !context.Activities.Any())
+            var users = userManager.Users.ToList();
+            if (!userManager.Users.Any())
             {
-                var users = new List<AppUser>
+                users = new List<AppUser>
                 {
                     new AppUser
                     {
@@ -41,12 +42,17 @@ namespace Persistence
                     await userManager.CreateAsync(user, "Pa$$w0rd");
                 }
 
+
+            }
+
+            if (!context.Activities.Any())
+            {
                 var activities = new List<Activity>
                 {
                     new Activity
                     {
                         Title = "Past Activity 1",
-                        Date = DateTime.Now.AddMonths(-2),
+                        Date = DateTime.UtcNow.AddMonths(-2),
                         Description = "Activity 2 months ago",
                         Category = "drinks",
                         City = "London",
@@ -63,7 +69,7 @@ namespace Persistence
                     new Activity
                     {
                         Title = "Past Activity 2",
-                        Date = DateTime.Now.AddMonths(-1),
+                        Date = DateTime.UtcNow.AddMonths(-1),
                         Description = "Activity 1 month ago",
                         Category = "culture",
                         City = "Paris",
@@ -85,7 +91,7 @@ namespace Persistence
                     new Activity
                     {
                         Title = "Future Activity 1",
-                        Date = DateTime.Now.AddMonths(1),
+                        Date = DateTime.UtcNow.AddMonths(1),
                         Description = "Activity 1 month in future",
                         Category = "music",
                         City = "London",
@@ -107,7 +113,7 @@ namespace Persistence
                     new Activity
                     {
                         Title = "Future Activity 2",
-                        Date = DateTime.Now.AddMonths(2),
+                        Date = DateTime.UtcNow.AddMonths(2),
                         Description = "Activity 2 months in future",
                         Category = "food",
                         City = "London",
@@ -129,7 +135,7 @@ namespace Persistence
                     new Activity
                     {
                         Title = "Future Activity 3",
-                        Date = DateTime.Now.AddMonths(3),
+                        Date = DateTime.UtcNow.AddMonths(3),
                         Description = "Activity 3 months in future",
                         Category = "drinks",
                         City = "London",
@@ -151,7 +157,7 @@ namespace Persistence
                     new Activity
                     {
                         Title = "Future Activity 4",
-                        Date = DateTime.Now.AddMonths(4),
+                        Date = DateTime.UtcNow.AddMonths(4),
                         Description = "Activity 4 months in future",
                         Category = "culture",
                         City = "London",
@@ -168,7 +174,7 @@ namespace Persistence
                     new Activity
                     {
                         Title = "Future Activity 5",
-                        Date = DateTime.Now.AddMonths(5),
+                        Date = DateTime.UtcNow.AddMonths(5),
                         Description = "Activity 5 months in future",
                         Category = "drinks",
                         City = "London",
@@ -190,7 +196,7 @@ namespace Persistence
                     new Activity
                     {
                         Title = "Future Activity 6",
-                        Date = DateTime.Now.AddMonths(6),
+                        Date = DateTime.UtcNow.AddMonths(6),
                         Description = "Activity 6 months in future",
                         Category = "music",
                         City = "London",
@@ -212,7 +218,7 @@ namespace Persistence
                     new Activity
                     {
                         Title = "Future Activity 7",
-                        Date = DateTime.Now.AddMonths(7),
+                        Date = DateTime.UtcNow.AddMonths(7),
                         Description = "Activity 7 months in future",
                         Category = "travel",
                         City = "Berlin",
@@ -234,7 +240,7 @@ namespace Persistence
                     new Activity
                     {
                         Title = "Future Activity 8",
-                        Date = DateTime.Now.AddMonths(8),
+                        Date = DateTime.UtcNow.AddMonths(8),
                         Description = "Activity 8 months in future",
                         Category = "drinks",
                         City = "London",
@@ -258,6 +264,8 @@ namespace Persistence
                 await context.Activities.AddRangeAsync(activities);
                 await context.SaveChangesAsync();
             }
+
+           
         }
     }
 }
