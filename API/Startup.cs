4c9d819891042be1844225dbeb038250e846e73e
SpacemanD <dimasforus@gmail.com>
2022-05-28 16:42:07 +0300

    update policy

--- API/Startup.cs
+++ API/Startup.cs
@@ -59,30 +59,12 @@ namespace API
             app.UseXXssProtection(opt => opt.EnabledWithBlockMode());
             app.UseXfo(opt => opt.Deny());
             app.UseCsp(opt => opt
-                .BlockAllMixedContent()
-                .StyleSources(s => s.Self().CustomSources(
-                    "https://fonts.googleapis.com",
-                    "sha256-/epqQuRElKW1Z83z1Sg8Bs2MKi99Nrq41Z3fnS2Nrgk=",
-                    "sha256-2aahydUs+he2AO0g7YZuG67RGvfE9VXGbycVgIwMnBI=",
-                    "sha256-+oGcdj5BhO6SoiIGYIkPOMYi7d2h2Pp/bkJLBfYL+kk="
-                ))
-                .FontSources(s => s.Self().CustomSources(
-                    "https://fonts.gstatic.com", "data:"
-                ))
-                .FormActions(s => s.Self())
-                .FrameAncestors(s => s.Self())
-                .ScriptSources(s => s.Self()
-                    .CustomSources(
-                        "sha256-HIgflxNtM43xg36bBIUoPTUuo+CXZ319LsTVRtsZ/VU=",
-                        "https://connect.facebook.net",
-                        "sha256-3x3EykMfFJtFd84iFKuZG0MoGAo5XdRfl3rq3r//ydA="
-                    ))
-                );
+                .BlockAllMixedContent());
 
             if (env.IsDevelopment())
             {
                 app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
             else
             {
