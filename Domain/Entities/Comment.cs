44b50f66e9466fa4b2aab9e91fe240c579f9fc09
honestexpert47 <dmgprokopiev@gmail.com>
2022-06-30 19:42:49 +0300

    US1: Updated entities relationship.

--- Domain/Entities/Comment.cs
+++ Domain/Entities/Comment.cs
@@ -2,7 +2,7 @@ namespace Domain.Entities
 {
     public class Comment
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         
         public string Body { get; set; }
         
